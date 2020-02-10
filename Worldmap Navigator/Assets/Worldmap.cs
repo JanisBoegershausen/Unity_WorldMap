653c2b3598ea368c1f42c6b9894efa9cba93300c
JanisBoegershausen <60890087+JanisBoegershausen@users.noreply.github.com>
2020-02-10 19:29:07 +0100

    Created Unity Project

---
+++ Worldmap Navigator/Assets/Worldmap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worldmap : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
